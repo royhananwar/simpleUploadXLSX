7ec2352ee66b926c279d7b8c6826f1036c77d7ae
Royhan Anwar <anwarroyhan@gmail.com>
2018-06-22 16:08:51 +0700

    Add project files.

---
+++ SimpleUpload/Controllers/UploadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleUpload.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Store()
+        //{
+
+        //}
+    }
+}
